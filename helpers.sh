e45ef622c72bdf0ae64d0b78f6ca8b90b9d23f4b
Kevin Kredit <knkredit60@gmail.com>
2022-05-26 00:44:02 -0400

    Add 'start' helper

---
+++ helpers.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+function joinlower {
+  local STR="$2"
+  for ELEM in "${@:3}"; do
+    STR="${STR}${1}${ELEM}"
+  done
+  echo $STR | awk '{print tolower($0)}'
+}
+
+function browse {
+  if uname -a | grep -q Darwin; then
+    open $@
+  else
+    xdg-open $@
+  fi
+}
+
+function start {
+  local SET=$1
+  local CHAL=$2
+
+  local BASEURL="https://cryptopals.com"
+  local SETURL="${BASEURL}/sets/$SET"
+  local CHALURL="${SETURL}/challenges/$CHAL"
+
+  local NAME="$(curl $SETURL 2>/dev/null | grep challenges/$CHAL | cut -d\> -f2 | cut -d\< -f1)"
+  local PROJ="$(joinlower "_" "cryptopals" $SET $CHAL $NAME)"
+
+  cargo new $PROJ
+  FILE="${PROJ}/src/main.rs"
+
+  cat > $FILE <<EOF
+// Cryptopals crypto challenge - set $SET challenge $CHAL
+// $CHALURL
+
+fn main() {
+}
+EOF
+
+  browse $CHALURL
+  git add $PROJ
+
+  $EDITOR $FILE
+}
